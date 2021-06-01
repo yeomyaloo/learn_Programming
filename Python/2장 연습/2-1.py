63ec6521eb3323e504a4f7145dbca513720bf3a6
hwiju yeom <yeom456@gmail.com>
2021-06-01 23:40:42 +0900

    d
    d

--- Python/2장 연습/2-1.py
+++ Python/2장 연습/2-1.py
@@ -1,20 +1,16 @@
-# 학생 5명의 시험 점수를 입력받아 합계와 평균을 출력하기
 
-print("학생 그룹 점수의 합계와 평균을 구합니다.")
 
-score1 = int(input("1번 학생의 점수를 입력하세요.: "))
-score2 = int(input("2번 학생의 점수를 입력하세요.: "))
-score3 = int(input("3번 학생의 점수를 입력하세요.: "))
-score4 = int(input("4번 학생의 점수를 입력하세요.: "))
-score5 = int(input("5번 학생의 점수를 입력하세요.: "))
+for _in range(1,4):
+    a =list(input())
 
-total = 0
 
-total += score1
-total += score2
-total += score3
-total += score4
-total += score5
 
-print(f"합계는 {total}점 입니다.")
-print(f"평균은 {total/5}점 입니다."
+def max_of(a):
+
+    maximum = a[0]
+    for i in range(1,len(a)):
+        if a[i]>maximum:
+            maximum = a[i]
+
+
+print(f"최댓값은 {a}입니다.")
