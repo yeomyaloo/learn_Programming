dd8626154eadae12505699102c5a95e763b7258f
hwiju yeom <81970382+yeomyaloo@users.noreply.github.com>
2021-09-13 22:29:45 +0900

    Update boj_1932.py

--- boj(baekjoon)/boj_1932.py
+++ boj(baekjoon)/boj_1932.py
@@ -9,10 +9,13 @@ for _ in range(n):
 
 for i in range(1,n):
     for j in range(len(tri[i])):
+        # 처음 각 라인의 처음과 끝 -> 바로 위의 숫자를 더해준다
         if j == 0:
             tri[i][j] = tri[i][j]+tri[i-1][j]
+        # 끝
         elif j == len(tri[i])-1:
             tri[i][j] = tri[i][j]+tri[i-1][j-1]
+        #나머지는 왼쪽 대각선, 오른쪽 대각선 중 최댓값을 더해나가 계속 누적시켜주면 된다.
         else:
             tri[i][j] =max(tri[i-1][j-1],tri[i-1][j])+tri[i][j]
 print(max(tri[n-1]))
