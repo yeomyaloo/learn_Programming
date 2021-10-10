d70c3c2191f6b2e2dbf50d8ad9adc54be1be444e
hwiju yeom <yeom456@gmail.com>
2021-10-10 14:50:40 +0900

    10/10

--- sw_ExpertAcademy/sw_ea_1288.py
+++ sw_ExpertAcademy/sw_ea_1288.py
@@ -1,16 +1,16 @@
+
 t = int(input())
 for tc in range(1, t + 1):
-    num = int(input())
+    n = int(input())
  
     arr = [False]*10
     j = 1
     cnt = 0
     while cnt != 10:
-        sleep = str(num*j)
+        sleep = str(n*j)
         for i in sleep:
-            #방문한 것을 방문처리해서 cnt + 1 해줌
             if arr[int(i)] == False:
                 arr[int(i)] = True
                 cnt += 1
         j += 1
-    print(f"#{tc} {num*(j-1)}")
+    print(f"#{tc} {n*(j-1)}")
--- sw_ExpertAcademy/sw_ea_1928.py
+++ sw_ExpertAcademy/sw_ea_1928.py
@@ -1,23 +1,10 @@
+import base64
 
 t = int(input())
 
 for tc in range(1,t+1):
-    testcase = int(input())
-    score = list(map(int,input().split()))
+    s = input()
+    enc = s.encode("UTF-8")
+    dec = base64.b64decode(enc)
 
-    #점수는 최대 100점    
-    cnt = [0]*101
-
-    #해당 점수가 몇번이나 나오는지 세기 위해 cnt 배열에 횟수 저장
-    for i in score:
-        cnt[i]+=1
-
-    m = max(cnt)
-    #최빈수가 여러개 존재할 때 점수가 더 큰 값을 출력해주기 위함 
-    print(f'#{tc} {max([i for i, v in enumerate(cnt) if v == m])}')
-
-    #enumerate
-    #1. 반복문을 사용할 때 몇 번째 반복문인지를 확인할 때 사용
-    #2. 인덱스 번호와 컬렉션의 원소를 튜플형태로 반환
-    
-    
+    print(f'#{tc} {dec.decode("UTF-8")}')
