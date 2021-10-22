3f2924dc7e57e51d4e7e11ec62fc5a366b516182
hwiju yeom <81970382+yeomyaloo@users.noreply.github.com>
2021-10-22 19:00:27 +0900

    10/22

---
+++ boj(baekjoon)/boj_10773.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    num = int(input())
+
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
