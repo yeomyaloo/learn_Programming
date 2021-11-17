06c7961d484a5cf53844991c5928c18d1d0fa2e4
hwiju yeom <yeom456@gmail.com>
2021-11-17 18:18:50 +0900

    11/17

---
+++ programmers/level_1 콜라츠 추측.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    
+    while n >1:
+
+            
+        if n % 2 == 0:
+            n = n//2 
+            answer += 1 
+        else:
+            n = (n*3)+1
+            answer += 1 
+    if answer > 500:
+        return -1
+    else:
+        return answer
