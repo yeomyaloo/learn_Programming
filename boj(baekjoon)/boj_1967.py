457ca54bfb9700b0e90129ba6703d54ec73771cb
hwiju yeom <81970382+yeomyaloo@users.noreply.github.com>
2022-01-10 15:29:24 +0900

    Update boj_1967.py

--- boj(baekjoon)/boj_1967.py
+++ boj(baekjoon)/boj_1967.py
@@ -32,7 +32,6 @@ for i in range(len(result1)):
         tmpmax=result1[i]
         index=i
  
-#최장경로 노드에서 다시 DFS를 통해 트리지름구하기
 result2=[0 for _ in range(n+1)]
 DFS(index,graph,result2)
 result2[index]=0
